d1d256c8fa6524db4600ad96cb8747d7be6e1950
Lukasz Dechnik <lukasz@dechnik.net>
2025-08-11 21:07:30 +0200

    Tweaking for my needs

--- install/config/config.sh
+++ install/config/config.sh
@@ -27,21 +27,21 @@ sudo sed -i 's|^\(auth\s\+required\s\+pam_faillock.so\)\s\+preauth.*$|\1 preauth
 sudo sed -i 's|^\(auth\s\+\[default=die\]\s\+pam_faillock.so\)\s\+authfail.*$|\1 authfail deny=10 unlock_time=120|' "/etc/pam.d/system-auth"
 
 # Set common git aliases
-git config --global alias.co checkout
-git config --global alias.br branch
-git config --global alias.ci commit
-git config --global alias.st status
-git config --global pull.rebase true
-git config --global init.defaultBranch master
+# git config --global alias.co checkout
+# git config --global alias.br branch
+# git config --global alias.ci commit
+# git config --global alias.st status
+# git config --global pull.rebase true
+# git config --global init.defaultBranch master
 
 # Set identification from install inputs
-if [[ -n "${OMARCHY_USER_NAME//[[:space:]]/}" ]]; then
-  git config --global user.name "$OMARCHY_USER_NAME"
-fi
-
-if [[ -n "${OMARCHY_USER_EMAIL//[[:space:]]/}" ]]; then
-  git config --global user.email "$OMARCHY_USER_EMAIL"
-fi
+# if [[ -n "${OMARCHY_USER_NAME//[[:space:]]/}" ]]; then
+#   git config --global user.name "$OMARCHY_USER_NAME"
+# fi
+# 
+# if [[ -n "${OMARCHY_USER_EMAIL//[[:space:]]/}" ]]; then
+#   git config --global user.email "$OMARCHY_USER_EMAIL"
+# fi
 
 # Set default XCompose that is triggered with CapsLock
 tee ~/.XCompose >/dev/null <<EOF
