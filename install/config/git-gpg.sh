2588bdb301844cffeecf5064326d117bea07d520
Lukasz Dechnik <lukasz@dechnik.net>
2025-08-18 18:22:08 +0200

    gnupg config

--- install/config/git-gpg.sh
+++ install/config/git-gpg.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+yay -S --noconfirm --needed pcsclite
+
 tee ~/.config/git/ignore >/dev/null <<EOF
 .idea
 .direnv
@@ -27,3 +29,63 @@ git config --global gpg.format openpgp
 git config --global gpg.program gpg2
 
 git config --global gpg.openpgp.program gpg
+
+mkdir -p ~/.gnupg
+
+tee ~/.gnupg/gpg-agent.conf >/dev/null <<EOF
+enable-ssh-support
+grab
+pinentry-program /usr/bin/pinentry-gnome3
+EOF
+
+tee ~/.gnupg/scdaemon.conf >/dev/null <<EOF
+pcsc-driver /usr/lib/libpcsclite.so
+card-timeout 5
+disable-ccid
+EOF
+
+tee ~/.gnupg/sshcontrol >/dev/null <<EOF
+EE6FCD5EF119342E3A679BBA23A2A6BD2AC7ACC6
+05095B28F0B05E3C2EA1820655692ED47A6EA731
+EOF
+
+tee ~/.gnupg/gpg.conf >/dev/null <<EOF
+cert-digest-algo SHA512
+charset utf-8
+default-preference-list SHA512 SHA384 SHA256 AES256 AES192 AES ZLIB BZIP2 ZIP Uncompressed
+fixed-list-mode
+keyid-format 0xlong
+list-options show-uid-validity
+no-comments
+no-emit-version
+no-symkey-cache
+personal-cipher-preferences AES256 AES192 AES
+personal-compress-preferences ZLIB BZIP2 ZIP Uncompressed
+personal-digest-preferences SHA512 SHA384 SHA256
+require-cross-certification
+s2k-cipher-algo AES256
+s2k-digest-algo SHA512
+trust-model tofu+pgp
+use-agent
+verify-options show-uid-validity
+with-fingerprint
+EOF
+
+tee ~/.config/systemd/user/link-gnupg-sockets.service >/dev/null <<EOF
+[Install]
+WantedBy=default.target
+
+[Service]
+ExecStart=/usr/bin/ln -Tfs /run/user/%U/gnupg %h/.gnupg-sockets
+ExecStop=/usr/bin/rm $HOME/.gnupg-sockets
+RemainAfterExit=true
+Type=oneshot
+
+[Unit]
+Description=link gnupg sockets from /run to /home
+EOF
+
+systemctl --user daemon-reload
+systemctl --user enable --now link-gnupg-sockets.service
+
+sudo systemctl enable --now pcscd
