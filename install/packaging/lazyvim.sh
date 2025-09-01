229d8c6e4551a89f7223a1e83a44cfa760e07080
Lukasz Dechnik <lukasz@dechnik.net>
2025-09-01 07:19:53 +0200

    Upstream install and lazyvim

--- install/packaging/lazyvim.sh
+++ install/packaging/lazyvim.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-if ! command -v nvim &>/dev/null; then
-  yay -S --noconfirm --needed neovim luarocks tree-sitter-cli
-
-  # Install LazyVim
-  rm -rf ~/.config/nvim
-fi
-
 if [[ ! -d "$HOME/.config/nvim" ]]; then
   git clone https://github.com/LazyVim/starter ~/.config/nvim
   cp -R ~/.local/share/omarchy/config/nvim/* ~/.config/nvim/
